94bfdbde20da2ef4beccc7e6c3255b6bd813829a
nunees <felipe_silva_nunes@hotmail.com>
2022-06-27 21:09:36 -0300

    feat: add categories model and how to save categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,29 @@
 import { Router } from "express";
 
+import { Category } from "../model/Category";
+
 const categoriesRoutes = Router();
 
-const categories = [];
+// Array that will hold our categories
+const categories: Category[] = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-  categories.push({
+
+  const category = new Category();
+
+  /**
+   * Assign atributes to an object that was created early.
+   * This way we dont need to manually assign values to objects,
+   * like category.name = name.
+   */
+  Object.assign(category, {
     name,
     description,
+    created_at: new Date(),
   });
+
+  categories.push(category);
 
   return response.status(201).json(categories);
 });
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import express from "express";
 import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
+
+// Tell express that we will use JSON
 app.use(express.json());
-app.use(categoriesRoutes);
+
+// Setting the categories argument, all the paths will start with "categories"
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("server is running..."));
