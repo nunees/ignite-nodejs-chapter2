5619aed8c90f069b2802d82687ca12fc50d8a67e
nunees <felipe_silva_nunes@hotmail.com>
2022-06-28 17:55:20 -0300

    feat: created model folder to organize code and implement specifications

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
 
 // Tell express that we will use JSON
 app.use(express.json());
 
+// Routes
 // Setting the categories argument, all the paths will start with "categories"
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(3333, () => console.log("server is running..."));
