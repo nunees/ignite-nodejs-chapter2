2b60201cb922fd47cd5d652d21e4536155ba3027
nunees <felipe_silva_nunes@hotmail.com>
2022-07-03 18:01:36 -0300

    feat: add documentation with swagger api

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 // Tell express that we will use JSON
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // Routes
 // Setting the categories argument, all the paths will start with "categories"
