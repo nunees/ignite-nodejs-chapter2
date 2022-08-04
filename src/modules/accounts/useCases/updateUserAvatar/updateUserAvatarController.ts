b392cbd9dae6aa8d68775eb140c497ab43cd25e7
nunees <felipe_silva_nunes@hotmail.com>
2022-08-04 20:32:01 -0300

    feat: add update avatar route

--- src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarController.ts
@@ -4,11 +4,9 @@ import { container } from "tsyringe";
 import { UpdateUserAvatarUseCase } from "./updateUserAvatarUseCase";
 
 export class UpdateUserAvatarController {
-  async handle(request: Request, response: Response) {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.user;
-
-    // Receive file
-    const avatar_file = null;
+    const avatar_file = request.file.filename;
 
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
     await updateUserAvatarUseCase.execute({ user_id: id, avatar_file });
