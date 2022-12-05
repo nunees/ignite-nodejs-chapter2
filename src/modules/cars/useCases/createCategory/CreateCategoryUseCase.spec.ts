fd9361a2076004bd7937eb5314626ddb8e220537
nunees <felipe_silva_nunes@hotmail.com>
2022-12-05 20:23:57 -0300

    test: createCar TDD initial files

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepositoryInMemory } from "@modules/cars/repositories/in-memory/InMemoryCategoriesRepository";
+import { CategoriesRepositoryInMemory } from "@modules/cars/repositories/in-memory/CategoriesRepositoryInMemory";
 import { AppError } from "@shared/errors/AppError";
 
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
