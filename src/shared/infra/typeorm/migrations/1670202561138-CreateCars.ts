f809a5872d79d2b9db678021d8309429d7886b08
nunees <felipe_silva_nunes@hotmail.com>
2022-12-04 22:30:14 -0300

    feat: add CreateCar database entry

---
+++ src/shared/infra/typeorm/migrations/1670202561138-CreateCars.ts
@@ -0,0 +1,76 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCars1670202561138 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "cars",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "daily_rate",
+            type: "numeric",
+          },
+          {
+            name: "available",
+            type: "boolean",
+            default: true,
+          },
+          {
+            name: "license_plate",
+            type: "varchar",
+          },
+          {
+            name: "fine_ammount",
+            type: "numeric",
+          },
+          {
+            name: "brand",
+            type: "varchar",
+          },
+          {
+            name: "category_id",
+            type: "uuid",
+            isNullable: false,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        // Foreign key reference
+        foreignKeys: [
+          {
+            // nome da referencia
+            name: "FKCategoryCar",
+            // Tabela de referencia
+            referencedTableName: "categories",
+            // coluna da tabela de referencia
+            referencedColumnNames: ["id"],
+            // Nome da coluna que ira usar a referencia
+            columnNames: ["category_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("cars");
+  }
+}
