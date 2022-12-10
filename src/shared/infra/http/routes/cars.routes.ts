3ad438ea451c99938926fc8f2a952fa638bfb4eb
nunees <felipe_silva_nunes@hotmail.com>
2022-12-10 19:16:25 -0300

    feat: add middleware to routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,10 +2,18 @@ import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 
+import { ensureAdmin } from "../Middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../Middlewares/ensureAuthenticated";
+
 const createCarController = new CreateCarController();
 
 const carsRoutes = Router();
 
-carsRoutes.post("/", createCarController.handle);
+carsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle
+);
 
 export { carsRoutes };
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -2,13 +2,18 @@ import { Router } from "express";
 
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 
+import { ensureAdmin } from "../Middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../Middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post("/", createSpecificationController.handle);
+specificationsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handle
+);
 
 export { specificationsRoutes };
