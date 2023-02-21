838ec0fecbd66ea3929a0bb3df58d3dc81d2aa42
nunees <felipe_silva_nunes@hotmail.com>
2023-02-21 18:34:45 -0300

    feat: add devolution rental controller

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -1,4 +1,4 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
@@ -11,11 +11,12 @@ interface IRequest {
   user_id: string;
 }
 
+@injectable()
 export class DevolutionRentalUseCase {
   constructor(
     @inject("RentalsRepository")
     private rentalsRepository: IRentalsRepository,
-    @inject("carsRepository")
+    @inject("CarsRepository")
     private carsRepository: ICarsRepository,
     @inject("DayjsDateProvider")
     private dateProvider: IDateProvider
@@ -23,7 +24,8 @@ export class DevolutionRentalUseCase {
 
   async execute({ id, user_id }: IRequest): Promise<Rental> {
     const rental = await this.rentalsRepository.findById(id);
-    const car = await this.carsRepository.findById(id);
+    const car = await this.carsRepository.findById(rental.car_id);
+
     const minimum_Daily = 1;
 
     if (!rental) {
