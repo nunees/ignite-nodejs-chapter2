85bc6904cadf6226a1c06a940cdf11f14639126f
nunees <felipe_silva_nunes@hotmail.com>
2023-02-21 18:56:26 -0300

    refactor: change Rental entity to return a car object

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -2,15 +2,23 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
   PrimaryColumn,
   UpdateDateColumn,
+  ManyToOne,
 } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
+
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 
 @Entity("rentals")
 export class Rental {
   @PrimaryColumn()
   id: string;
+
+  @ManyToOne(() => Car)
+  @JoinColumn({ name: "car_id" })
+  car: Car;
 
   @Column()
   car_id: string;
