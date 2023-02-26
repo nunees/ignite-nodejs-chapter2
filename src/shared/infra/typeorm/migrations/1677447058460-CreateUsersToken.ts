0f8114e5526f7c511d000ace89082a98b6b5fc68
nunees <felipe_silva_nunes@hotmail.com>
2023-02-26 18:40:51 -0300

    feat: add create users token migration file

---
+++ src/shared/infra/typeorm/migrations/1677447058460-CreateUsersToken.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsersToken1677447058460 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users_token",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "refresh_token",
+            type: "varchar",
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "expires_date",
+            type: "timestamp",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKUserToken",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users_tokens");
+  }
+}
