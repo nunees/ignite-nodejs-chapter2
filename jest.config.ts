21a7cd49c2983625d12b904435f98d9bbc63a230
nunees <felipe_silva_nunes@hotmail.com>
2023-04-10 20:10:40 -0300

    test: add coverage report

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,10 @@ export default {
   bail: true,
   clearMocks: true,
   coverageProvider: "v8",
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/modules/**/useCases/**/*.ts"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text-summary", "lcov"],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: "<rootDir>/src",
   }),
